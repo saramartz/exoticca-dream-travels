397c620b290574abc85fc2ca4c66ac03b8b7dab9
Sara Martinez Vega <saramartz_@hotmail.com>
2024-07-04 19:05:18 +0200

    create project boilerplate, include global styles and favicon

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
 
-export default nextConfig;
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url) // get the resolved path to the file
+const __dirname = path.dirname(__filename) // get the name of the directory
+
+const nextConfig = {
+    sassOptions: {
+        includePaths: [`${__dirname}/src/ui/styles`],
+        prependData: '@import "abstracts";', // make sass variables available to all css modules files
+    },
+}
+
+export default nextConfig
---
+++ src/ui/components/common/Card/Card.tsx
@@ -0,0 +1,8 @@
+const Card = () => {
+
+    return (
+        <div>Card component</div>
+    )
+}
+
+export default Card
